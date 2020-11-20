ac0b8e3982279b67195ee6d9ed75ee8642c23b0e
mirsaitsabirzanov <mirsait77777@gmail.com>
2020-11-20 20:51:38 +0300

    first commit

---
+++ app/src/main/java/com/example/fin_reg/Teacher_java/TeacherMainActivity.java
@@ -0,0 +1,34 @@
+package com.example.fin_reg;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class TeacherMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
+        NavController navController = Navigation.findNavController(TeacherMainActivity.this, R.id.nav_host_fragment);
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+//        NavigationUI.setupActionBarWithNavController(this, navController);
+
+    }
+//
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        onBackPressed();
+//        return super.onSupportNavigateUp();
+//    }
+}
---
+++ app/src/main/java/com/example/fin_reg/Teacher_java/Teacher_profile.java
@@ -0,0 +1,34 @@
+package com.example.fin_reg;
+
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Teacher_profile extends Fragment {
+
+
+    public Teacher_profile() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.activity_teacher_profile, container, false);
+    }
+
+}
